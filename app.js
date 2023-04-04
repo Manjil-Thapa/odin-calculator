84a2fb73c0f8ce38ca54c37760b1757d9a27ea2c
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-04-05 00:31:38 +1000

    added basic funtionality

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const container = document.querySelector("#main-div");
+const calculator = document.querySelector("#calculator");
 
 const buttons = [
   "%",
-  "CE",
   "C",
-  "&#8678;",
-  "<sup>1</sup>/<sub>2</sub>",
-  "x<sup>2</sup>",
-  "&radic;",
+  "<-",
   "/",
   "7",
   "8",
   "9",
-  "&#215;",
+  "*",
   "4",
   "5",
   "6",
@@ -26,30 +22,104 @@ const buttons = [
   ".",
   "=",
 ];
-buttons.forEach((btn) => {
-  const keys = document.createElement("button");
-  keys.classList.add("btn");
-  keys.innerHTML = btn;
-  container.append(keys);
+
+buttons.forEach((key) => {
+  const btn = document.createElement("input");
+  btn.setAttribute("type", "button");
+  btn.setAttribute("value", key);
+  btn.classList.add("btn");
+  btn.onclick = handleClick;
+  calculator.append(btn);
 });
 
-//Keys selection
-const keys = document.querySelectorAll(".btn");
-keys.addEventListener("click", function () {});
+//Calculator functionality
+const display = document.querySelector("#display");
+let operand1 = null;
+let operand2 = null;
+let operator = null;
 
 // Functions
+function handleClick() {
+  let value = this.getAttribute("value");
+  //   console.log(value);
+  switch (value) {
+    case "C":
+      operand1 = null;
+      operand2 = null;
+      operator = null;
+      display.value = "";
+      break;
 
-// add
-const add = function (num1, num2) {
-  return num1 + num2;
-};
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      if (operator === null) {
+        operator = value;
+        display.value += value;
+      }
+      break;
 
-// subtract
-const subtract = function (num1, num2) {
-  return num1 - num2;
-};
+    case ".":
+      if (operator === null) {
+        if (operand1 === null) {
+          operand1 = "0.";
+        } else if (operand1.indexOf(".") === -1) {
+          operand1 += ".";
+        }
+      } else {
+        if (operand2 === null) {
+          operand2 = "0.";
+        } else if (operand2.indexOf(".") === -1) {
+          operand2 += ".";
+        }
+      }
+      display.value += ".";
+      break;
 
-// summation
-const sum = function (nums) {
-  return nums.reduce((total, current) => total + current, 0);
-};
+    case "=":
+      if (operator !== null && operand1 !== null && operand2 !== null) {
+        let result = getResult(
+          parseFloat(operand1),
+          operator,
+          parseFloat(operand2)
+        );
+        display.value = result;
+        operand1 = result.toString();
+        operator = null;
+        operand2 = null;
+      }
+      break;
+
+    default:
+      if (operator === null) {
+        if (operand1 === null) {
+          operand1 = value;
+        } else {
+          operand1 += value;
+        }
+      } else {
+        if (operand2 === null) {
+          operand2 = value;
+        } else {
+          operan2 += value;
+        }
+      }
+      display.value += value;
+  }
+}
+
+function getResult(operand1, operator, operand2) {
+  switch (operator) {
+    case "+":
+      return operand1 + operand2;
+    case "-":
+      return operand1 - operand2;
+    case "*":
+      return operand1 * operand2;
+    case "/":
+      return operand1 / operand2;
+    default:
+      return null;
+  }
+}
